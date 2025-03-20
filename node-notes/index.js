5ea499b06ce6ade4423142e5887ebe930e652abd
Emilea Chin <emileachinn@gmail.com>
2025-03-20 03:02:49 +0000

    front-end deployment

--- node-notes/index.js
+++ node-notes/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(express.static('dist'))
+app.use(cors())
 
 let notes = [
     {
@@ -82,7 +86,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
