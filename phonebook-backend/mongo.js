b625a7c6379079e1f42d254b94d68242509b66a1
Emilea Chin <emileachinn@gmail.com>
2025-03-21 02:37:32 +0000

    3.12

--- phonebook-backend/mongo.js
+++ phonebook-backend/mongo.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
-if(process.argv.length < 5) {
-    console.log('enter password, name and number')
+if(process.argv.length < 3) {
+    console.log('enter password')
     process.exit(1)
 }
 
 const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 
-const url = `mongodb+srv://<db_username>:${password}@phonebook.3rowi.mongodb.net/Persons?retryWrites=true&w=majority&appName=Phonebook`
+const url = `mongodb+srv://emileachinn:${password}@phonebook.3rowi.mongodb.net/Persons?retryWrites=true&w=majority&appName=Phonebook`
 
 mongoose.set('strictQuery',false)
 
@@ -21,11 +23,25 @@ const personSchema = new mongoose.Schema({
 const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
-    name: process.argv[3],
-    number: process.argv[4]
+    name: name,
+    number: number
 })
 
+if (process.argv.length == 5)
+{
 person.save().then(result => {
-    console.log(`added ${result.name} ${result.number} to phonebook`)
+    console.log(`added ${name} ${number} to phonebook`)
     mongoose.connection.close()
 })
+}
+
+if (process.argv.length == 3)
+{
+Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+        console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+})
+}
