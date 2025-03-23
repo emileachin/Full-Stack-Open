b4873a440d41ad8bdebddb5eb10f9879a3cc867a
Emilea Chin <emileachinn@gmail.com>
2025-03-23 08:01:39 +0000

    4.1-4.7

---
+++ part_4/bloglist/utils/list_helper.js
@@ -0,0 +1,51 @@
+const lodash = require('lodash')
+
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = blogs => {
+  return blogs.reduce((acc, curr) => acc + curr.likes, 0)
+}
+
+const favoriteBlog = blogs => {
+  let max = 0
+  let index = 0
+
+  for (let i = 0; i < blogs.length; i++)
+  {
+    if(blogs[i].likes > max)
+    {
+      max = blogs[i].likes
+      index = i
+    }
+}
+return blogs[index]
+}
+
+const mostBlogs = blogs => {
+  const matchingAuthors = lodash.groupBy(blogs, 'author')
+  const matchingAuthorsList = lodash.map(matchingAuthors, (blogs, author) => ({
+    author: author,
+    blogs: blogs.length,
+  }))
+
+
+  return lodash.maxBy(matchingAuthorsList, 'blogs')
+}
+
+const mostLikes = blogs => {
+  const allAuthors = lodash.groupBy(blogs, 'author')
+  const likes = lodash.map(allAuthors, (blogs, author) => {
+    return {
+      author: author,
+      likes: lodash.sumBy(blogs, 'likes')
+    }
+  })
+  
+  return lodash.maxBy(likes, 'likes')
+}
+  
+module.exports = {
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
+}
