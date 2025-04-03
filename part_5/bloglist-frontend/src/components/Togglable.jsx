ea98ba4f804a2d96ab0dec158813624454b58bc9
Emilea Chin <emileachinn@gmail.com>
2025-04-03 00:25:02 +0000

    end of 5b

--- part_5/bloglist-frontend/src/components/Togglable.jsx
+++ part_5/bloglist-frontend/src/components/Togglable.jsx
@@ -1,6 +1,7 @@
 import { useState, forwardRef, useImperativeHandle } from 'react'
+import PropTypes from 'prop-types'
 
-const Togglable = forwardRef((props, refs) => {
+const Togglable = React.forwardRef((props, refs) => {
     const [visible, setVisible] = useState(false)
 
     const hideWhenVisible = { display : visible ? 'none' : ''}
@@ -26,5 +27,11 @@ const Togglable = forwardRef((props, refs) => {
         </div>
     )
 })
+
+Togglable.displayName = 'Togglable'
+
+Togglable.propTypes = {
+    buttonLabel: PropTypes.string.isRequired
+}
 
 export default Togglable 
