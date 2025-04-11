21a985c8dcc9bf403830963d8810d613dc5c869d
Emilea Chin <emileachinn@gmail.com>
2025-04-11 05:35:31 +0000

    6a - 6b

---
+++ part_6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,53 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { createNotification } from '../reducers/notificationReducer'
+
+
+const Anecdote = ({ anecdote, handleClick }) => {
+ return (
+   <div key={anecdote.id} className="anecdote">
+     <div>{anecdote.content}</div>
+     <div>
+       has {anecdote.votes}
+       <button onClick={handleClick}>vote</button>
+     </div>
+   </div>
+ )
+}
+
+
+const AnecdoteList = () => {
+ const dispatch = useDispatch()
+
+
+ const vote = (id) => {
+   dispatch(voteAnecdote(id))
+   dispatch(createNotification(`you voted for '${anecdotes.find((a) => a.id === id).content}'`))
+ }
+
+
+ const anecdotes = useSelector(({ filter, anecdotes }) => {
+   if (filter === '') {
+     return anecdotes
+   }
+
+
+   return anecdotes.filter((anecdote) => {
+     return anecdote.content.toLowerCase().includes(filter.toLowerCase())
+   })
+ })
+
+
+ return [...anecdotes]
+   .sort((a, b) => b.votes - a.votes)
+   .map((anecdote) => (
+     <Anecdote
+       key={anecdote.id}
+       anecdote={anecdote}
+       handleClick={() => vote(anecdote.id)}
+     />
+   ))
+}
+
+
+export default AnecdoteList
---
+++ part_6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+const initialState = null
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState,
+   reducers: {
+     setNotification(state, action) {
+        return action.payload
+     }
+   }
+})
+export const createNotification = (notif, secs = 5000) => {
+  return async (dispatch) => {
+    dispatch(setNotification(notif))
+    setTimeout(() => {
+      dispatch(setNotification(null))
+    }, secs)
+  }
+}
+export const { setNotification } = notificationSlice.actions
+export default notificationSlice.reducer
